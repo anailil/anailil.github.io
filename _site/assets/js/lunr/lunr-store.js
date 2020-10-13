07450e95506b1976acf6bc4f566ab3a5d60b0351
LVM <liliana.meleza@gmail.com>
2020-10-13 16:01:36 +0200

    removed post entries

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,25 +1,7 @@
 var store = [{
-        "title": "Salt characterisation",
-        "excerpt":"Salt geometry is difficult to image seismically due to: Large velocity contrast Steeply dipping flanks Most algorithms assume acoustic (isotropic and homogeneous) media However, observations of natural salt show layering, like this photo from a salt mine in Canada where I conducted fieldwork for my PhD project. Now, salt is...","categories": [],
-        "tags": ["post","geo","field","lab"],
-        "url": "http://localhost:4000/Salt-characterisation/",
-        "teaser": null
-      },{
-        "title": "Discontinuity attributes",
-        "excerpt":"Introduction Discontinuity calculations, also called coherence or semblance, are some of the most commonly used seismic attributes. They measure the amount of similarity between adjacent seismic traces. However, there are several different types of discontinuity estimates and a plethora of names for similar attributes. Cross-correlation The earliest discontinuity algorithm used...","categories": [],
-        "tags": ["post","geo","seismic","attributes"],
-        "url": "http://localhost:4000/Discontinuity-attributes/",
-        "teaser": null
-      },{
-        "title": "Sedimentary facies classification",
-        "excerpt":"The following sequence of images show results from a workflow to processes seismic cross-sections. Seismic cross-section showing amplitude (frequency band modulated) Seismic cross-section showing the coherence attribute (discontinuity) from original data. Seismic cross-section showing the coherence attribute (discontinuity) smoothed with kuwahara filter. Seismic cross-section showing the maximum magnitud (Fourier). Seismic...","categories": [],
-        "tags": ["post","geo","seismic","attributes"],
-        "url": "http://localhost:4000/Sedimentary-facies-classification/",
-        "teaser": null
-      },{
-        "title": "Visualising borehole logs",
+        "title": "Quick visualisation of geophysical borehole data",
         "excerpt":"Geophysical well logs: Quick visualisation and interpretation Among geophysical data, well logs are considered hard data because they contain geophysical measurements of the stratigraphic sequence registered along a wellpath. Well logs are useful to calibrate rock properties derived from seismic data. The following steps provide a simple way for displaying...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Visualising-borehole-logs/",
+        "url": "http://localhost:4000/Quick-Visualisation-of-Geophysical-Borehole-Data/",
         "teaser": null
       }]
